06148edaee9a6f99f1dad61e55d9e2de9fb2267d
Ruqaya Husain <ruqayanader22@gmail.com>
2025-11-28 23:59:00 +0300

    solved all exs

--- exercises.py
+++ exercises.py
@@ -123,3 +123,53 @@ def calculate_tip(bill_amount, tip):
 
 
 print('Exercise 7:', calculate_tip(50, 20))
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+def product(*args):
+    total = 1
+    for arg in args:
+        total *= arg
+    return total
+
+
+print('Exercise 8:', product(2, 5, 5))
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basic_calculator` that takes three arguments: 
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basic_calculator(10, 5, 'subtract') should return 5.
+# basic_calculator(10, 5, 'add') should return 15.
+# basic_calculator(10, 5, 'multiply') should return 50.
+# basic_calculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+def basic_calculator(first_operand, second_operand, operation):
+    operation = operation.lower()
+
+    if operation == 'add':
+        return first_operand + second_operand
+    elif operation == 'subtract':
+        return first_operand - second_operand
+    elif operation == 'multiply':
+        return first_operand * second_operand
+    elif operation == 'divide':
+        return first_operand // second_operand
+    else:
+        return
+
+
+print('Exercise 9 Result:', basic_calculator(10, 5, 'subtract'))
